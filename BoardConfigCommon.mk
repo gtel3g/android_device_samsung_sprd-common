ac45c5b677734cd9e17130ef4e36ed8670b7fbb8
Ma3a_XaTa <v.radulevich@gmail.com>
2026-08-27 22:32:53 +0300

    sprd-common: remove obsolete CM hardware class

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -26,7 +26,6 @@
 # SPRD hardware
 BOARD_USES_SPRD_HARDWARE := true
 
-
 # RIL
 BOARD_PROVIDES_LIBRIL := true
 
@@ -37,6 +36,3 @@ BOARD_HAVE_BLUETOOTH_BCM := true
 # Audio
 BOARD_USES_TINYALSA_AUDIO := true
 TARGET_TINY_ALSA_IGNORE_SILENCE_SIZE := true
-
-# CMHW
-BOARD_HARDWARE_CLASS := device/samsung/sprd-common/cmhw/
